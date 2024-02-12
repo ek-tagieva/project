bad0fe08d8d7f8564af89399958f0e9681890daa
Ekaterina Tagieva <ek.tagieva@gmail.com>
2024-02-12 13:19:32 +0300

    fix commit

---
+++ project4/mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fc6521c9123e28c56039b0bedbec795b77eaac14',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fc6521c9123e28c56039b0bedbec795b77eaac14',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
